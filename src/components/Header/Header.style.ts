a639c134589f938e65942cdda520720cca8d2758
Tuan Pham <tuan.pham@avigilon.com>
2019-12-16 17:24:35 -0800

    wip

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,11 @@
+const headerStyle = () => {
+  const style = {
+    wrapper: {
+      backgroundColor: 'red',
+    }
+  };
+
+  return style;
+}
+
+export default headerStyle;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+
+import headerStyle from './Header.style';
+
+function Header({ classes }) {
+  return (
+    <div>
+      Discus
+    </div>
+  )
+}
+
+export default injectSheet(headerStyle)(Header);
